df6205450dc1acfda01b05a82c989895a2de3018
root <you@example.com>
2023-10-02 17:10:40 +0200

    update file, add bash scripts test setup checking localstack s3 api

---
+++ bash/download-s3-object.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+aws configure set aws_access_key_id test
+aws configure set aws_secret_access_key test
+aws configure set default.region us-east-1
+# AWS S3: Port 4572 (HTTP) and Port 4573 (HTTPS) - Used to emulate Amazon S3.
+aws configure set default.endpoint_url http://localstack:4566
+# Option 1a: Set the AWS_PAGER environment variable to an empty string
+export AWS_PAGER=""
+aws s3 cp s3://mybucket/sample.txt sample-download.txt
