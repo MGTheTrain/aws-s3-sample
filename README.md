4c93bf704ef05807f972cc521686df6b1a9acf73
Marvin Gajek <Marvin.Gajek1@web.de>
2023-10-29 10:54:38 +0100

    update README.md, add cargo tests, add output folder, move

--- README.md
+++ README.md
@@ -18,27 +18,54 @@ Repository demonstrating how to manage blobs in AWS S3 service buckets with Rust
 
 ## How to use
 
-**0. Run the docker compose cluster to have an localstack docker container locally running:**
+### Rust
+
+The [Rust sample](./src/main.rs) can be started with `cargo run`. Please note that the localstack docker container s3 service can not be used for local blob management tests (up-, download, deletion, metadata retrieval, etc.) on Windows 10 OS. You need to utilize a public s3 service bucket.
+
+Therefore create from the [secrets.template.cfg](./temp/secrets.template.cfg) a `secrets.cfg` file and replace the `<PLACEHOLDER_*>` values.
+Afterwards execute the following to run the tests:
+
+```bash
+cargo test
+# cosidering logs execute
+RUST_LOG=info cargo test
+```
+
+In order to build and run the main executable run:
+
+```bash
+cargo build
+
+# Example upload: 
+cargo.exe run -- upload -b blob.txt  -u sample.txt
+cargo.exe run -- upload --blob-name blob.txt --upload-file-path sample.txt 
+
+# Example download: 
+cargo.exe run -- download -b blob.txt -d output/download.txt
+cargo.exe run -- download --blob-name blob.txt --download-file-path "output/download.txt"
+
+# Example delete: 
+cargo.exe run -- delete -b blob.txt
+cargo.exe run -- delete --blob-name blob.txt
+
+# or running the executable  
+cp target/debug/aws-s3-storage .
+source secrets.cfg
+./aws-s3-storage --help
+# Example upload (Note: Colored crates console logs might not work on certain terminals): 
+./aws-s3-storage upload --blob-name blob.txt --upload-file-path sample.txt 
+```
+
+### Optional
+
+#### Run the docker compose cluster to have an localstack docker container locally running
 
 ```bash
 sudo docker compose up -d --build
 ```
 
-**1. Run tests**
+#### Run tests
 
 | Scripting language | Description | 
 |----------|----------|
 | Bash | Navigate to the [bash scripts](./scripts/bash/) folder. Go trough the comments in the Dockerfile and utilize it as a reference for testing blob up- and downloads to/from the localstack docker container s3 service. | 
-
-**2. Run Rust sample**
-
-The [Rust sample](./src/main.rs) can be started with `cargo run`. Please note that the localstack docker container s3 service can not be used for local blob management tests (up-, download, deletion, metadata retrieval, etc.) on Windows 10 OS. You need to utilize a public s3 service bucket.
-
-Therefore create from the [secrets.template.cfg](./temp/secrets.template.cfg) a `secrets.cfg` file and replace the `<PLACEHOLDER_*>` values.
-Afterwards execute the following:
-
-```bash
-source temp/secrets.cfg
-cargo run
-```
-
--- src/main.rs
+++ src/main.rs
@@ -4,68 +4,85 @@ use std::fs;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // Define a list of environment variable names to check
-    let env_vars_to_check = [ "AWS_ACCESS_KEY_ID",
-                                         "AWS_SECRET_ACCESS_KEY",
-                                         "AWS_DEFAULT_REGION",
-                                         "AWS_ENDPOINT_URL",
-                                         "AWS_BUCKET_NAME" ];
-
-    // Call the function to check if the environment variables are set
-    if are_env_vars_set(&env_vars_to_check) {
-        println!("All environment variables are set.");
-    } else {
-        println!("Some or all environment variables are not set.");
-    }
-
-    // Get default credentials
-    let config = aws_config::load_from_env().await;
-
-    // Create an client client
-    let client = Client::new(&config);
-
-    let mut region = String::from("eu-central-1");
-    match env::var("AWS_DEFAULT_REGION") {
-        Ok(value) => {
-            // println!("Value of {} is: {}", region, value);
-            region = value;
-        }
-        Err(_) => {
-            println!("{} is not set.", region);
-        }
-    }
-    let bucket_name =
-            std::env::var("AWS_BUCKET_NAME").expect("AWS_BUCKET_NAME environment variable expected");
-    
-    let key = "sample.txt";
-    let file_name = "sample.txt";
-    let file_path = "temp/sample-copy.txt";
-
-    show_buckets(&client).await?;
-
-    create_bucket(&client, &bucket_name, &region).await?;
-    println!("Created bucket with name {}", bucket_name);
-    
-    upload_object(&client, &bucket_name, &file_name, &key).await?;
-    println!("Uploaded file {} with object name {} to bucket with name {}", file_name, key, bucket_name);
-
-    // Download
-    let get_object_output = get_object(&client, &bucket_name, &key).await?;
-    let data = get_object_output.body.collect().await.unwrap().into_bytes();
-    
-    let contents = std::str::from_utf8(&data).unwrap(); // Note that this code assumes that the files are utf8 encoded plain text format.
-    println!("Key: {key}, Contents: {contents}");
-
-    write_bytes_to_file(&data, &file_path).await; 
-
-    remove_object(&client, &bucket_name, &key).await?;
-
-    delete_bucket(&client, &bucket_name).await?;
-
     Ok(())
 }
 
-// Helper methds
+#[cfg(test)]
+mod tests {
+    use std::env;
+
+    use aws_sdk_s3::{Client, Error};
+
+    use crate::{are_env_vars_set, show_buckets, create_bucket, upload_object, get_object, write_bytes_to_file, remove_object, delete_bucket};
+
+    // In order to run the test execute: `RUST_LOG=info cargo test`
+    #[tokio::test]
+    async fn test_aws_s3_storage_methods() -> Result<(), Error> {
+        env_logger::init();
+        
+        let env_file_path = "secrets.cfg";
+        dotenv::from_path(env_file_path).ok();
+
+        // Define a list of environment variable names to check
+        let env_vars_to_check = [ "AWS_ACCESS_KEY_ID",
+                                             "AWS_SECRET_ACCESS_KEY",
+                                             "AWS_DEFAULT_REGION",
+                                             "AWS_ENDPOINT_URL",
+                                             "AWS_BUCKET_NAME" ];
+
+        // Call the function to check if the environment variables are set
+        if are_env_vars_set(&env_vars_to_check) {
+            println!("All environment variables are set.");
+        } else {
+            println!("Some or all environment variables are not set.");
+        }
+
+        // Get default credentials
+        let config = aws_config::load_from_env().await;
+
+        // Create an client client
+        let client = Client::new(&config);
+
+        let mut region = String::from("eu-central-1");
+        match env::var("AWS_DEFAULT_REGION") {
+            Ok(value) => {
+                // println!("Value of {} is: {}", region, value);
+                region = value;
+            }
+            Err(_) => {
+                println!("{} is not set.", region);
+            }
+        }
+        let bucket_name =
+                std::env::var("AWS_BUCKET_NAME").expect("AWS_BUCKET_NAME environment variable expected");
+        
+        let key = "sample.txt";
+        let file_name = "sample.txt";
+        let file_path = "output/sample-copy.txt";
+
+        assert!(show_buckets(&client).await.is_ok());
+        assert!(create_bucket(&client, &bucket_name, &region).await.is_ok());
+        println!("Created bucket with name {}", bucket_name);
+        
+        assert!(upload_object(&client, &bucket_name, &file_name, &key).await.is_ok());
+        println!("Uploaded file {} with object name {} to bucket with name {}", file_name, key, bucket_name);
+
+        // Download
+        let get_object_output = get_object(&client, &bucket_name, &key).await;
+        assert!(get_object_output.is_ok());
+        let data = get_object_output?.body.collect().await.unwrap().into_bytes();
+        
+        let contents = std::str::from_utf8(&data).unwrap(); // Note that this code assumes that the files are utf8 encoded plain text format.
+        println!("Key: {key}, Contents: {contents}");
+
+        assert!(write_bytes_to_file(&data, &file_path).await.is_ok()); 
+        assert!(remove_object(&client, &bucket_name, &key).await.is_ok());
+        assert!(delete_bucket(&client, &bucket_name).await.is_ok());
+
+        Ok(())
+    }
+}
+
 
 fn are_env_vars_set(env_var_names: &[&str]) -> bool {
     let mut all_set = true;
